f0d57d57f8c521d9e722f30adec7b69086409ead
Fide <rolondavid@outlook.com>
2022-07-19 21:53:09 -0300

    POO_7 ejercicio8

---
+++ POO_7/ejercicio9/ejercicio9.java
@@ -0,0 +1,13 @@
+package POO_7.ejercicio9;
+
+import java.util.Calendar;
+
+public class ejercicio9 {
+    public static void main(String[] args) {
+        //Creo objeto Calendar para obtener la fecha actual
+        Calendar calendario = Calendar.getInstance();
+        System.out.println(calendario.getTime());
+
+    }
+
+}
