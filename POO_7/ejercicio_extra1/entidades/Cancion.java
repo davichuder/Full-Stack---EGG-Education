655209f5bd2ae72ff8e529f2e661f26b9a33c42d
Fide <rolondavid@outlook.com>
2022-07-21 20:34:52 -0300

    POO_7 ejercicio_extra2

---
+++ POO_7/ejercicio_extra1/entidades/Cancion.java
@@ -0,0 +1,38 @@
+package POO_7.ejercicio_extra1.entidades;
+
+public class Cancion {
+    //Parametros
+    private String titulo;
+    private String autor;
+
+    //Getters y Setters
+    public String getTitulo() {
+        return this.titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return this.autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    //Constructor vacio
+    public Cancion(){
+        this.titulo = "";
+        this.autor = "";
+    }
+
+    //Constructor con parametros
+    public Cancion(String titulo, String autor){
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+
+
+}
