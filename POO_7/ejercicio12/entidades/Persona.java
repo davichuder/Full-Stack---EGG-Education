36949704db05cd27107a329dc809206b3a80bf4f
Fide <rolondavid@outlook.com>
2022-07-20 21:56:35 -0300

    POO_7 ejercicio12

---
+++ POO_7/ejercicio12/entidades/Persona.java
@@ -0,0 +1,57 @@
+package POO_7.ejercicio12.entidades;
+
+import java.util.Date;
+
+public class Persona {
+    //Parametros
+    private String nombre;
+    private Date fechaNacimiento;
+
+    //Getters and Setters
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Date getFechaNacimiento() {
+        return this.fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+    
+    //Constructor por defecto
+    public Persona() {}
+
+    //Constructor parametrizado
+    public Persona(String nombre, Date fechaNacimiento) {
+        this.nombre = nombre;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    //Metodo calcularEdad
+    public int calcularEdad() {
+        Date fecha_actual = new Date();
+        int diferencia = (int) ((fecha_actual.getTime() - fechaNacimiento.getTime())/1000/60/60/24/365);
+        return diferencia;
+    }
+
+    //Metodo menorQue
+    public boolean menorQue(int edad) {
+        if (edad > this.calcularEdad()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    //Metodo mostrarPersona
+    public void mostrarPersona() {
+        System.out.println("Nombre: " + this.nombre);
+        System.out.println("Fecha de nacimiento: " + this.fechaNacimiento);
+    }
+}
---
+++ POO_7/ejercicio12/servicios/PersonaServicio.java
@@ -0,0 +1,31 @@
+package POO_7.ejercicio12.servicios;
+
+import java.util.Date;
+import java.util.Scanner;
+
+import POO_7.ejercicio12.entidades.Persona;
+
+public class PersonaServicio {
+    public Persona crearPersona() {
+        //Creo variables
+        String nombre;
+        Date fechaNacimiento;
+        int dia, mes, anio;
+        Scanner scanner = new Scanner(System.in);
+        //Pido datos
+        System.out.println("Introduce el nombre: ");
+        nombre = scanner.nextLine();
+        System.out.println("Introduce la fecha de nacimiento: ");
+        System.out.println("Introduce el dia: ");
+        dia = scanner.nextInt();
+        System.out.println("Introduce el mes: ");
+        mes = scanner.nextInt();
+        System.out.println("Introduce el anio: ");
+        anio = scanner.nextInt();
+        //Creo fecha
+        fechaNacimiento = new Date(anio-1900, mes-1, dia);
+        //Creo persona
+        Persona persona = new Persona(nombre, fechaNacimiento);
+        return persona;
+    }    
+}
